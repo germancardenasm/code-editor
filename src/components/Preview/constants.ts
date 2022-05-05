e2f658ed7086bb7998dff2eea559d850c85e2d87
German Cardenas <germancardenas@gmail.com>
2022-05-05 18:02:29 -0500

    GCM 7: improve code

---
+++ src/components/Preview/constants.ts
@@ -0,0 +1,25 @@
+export const iframeInitHtml = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div id="root"></div>
+    <script> 
+      window.addEventListener(
+        'message',
+        (event) => {
+          try {
+            eval(event.data);
+          } catch (e) {
+            console.error(e);
+          }
+        },
+        false
+      );
+    </script> 
+  </body> 
+</html>
+`;
